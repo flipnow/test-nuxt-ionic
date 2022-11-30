a8eeb9eee1efc10301815dd5eea80aa000f38801
Filip Nowakowski <filip.nowakowski@monterail.com>
2022-11-30 10:09:01 +0100

    fix: add seo composable

--- app.vue
+++ app.vue
@@ -5,25 +5,12 @@
 </template>
 
 <script setup lang="ts">
-import { useCounterStore } from '@/stores/counterStore';
-import { Event } from '@/types/LiveEvents';
-import { storeToRefs } from 'pinia';
 try {
-  const route = useRoute();
-
-  const { events } = storeToRefs(useCounterStore());
-
-  if (route.name === 'Test') {
-    const data = await $fetch(`${import.meta.env.VITE_API_URL}/search/liveEvents`, {
-      body: { eventIds: 'ugn29046xzrbvyv0fbif9' },
-      method: 'POST',
-    });
-
-    events.value = (data as { events: { rows: [Event] } })?.events?.rows;
-
-    useHead({
-      title: events.value?.[0]?.event?.eventName ?? 'Head',
-    });
+  if (process.server) {
+    const { setHead } = useSeoMeta();
+    const head = await setHead();
+    console.log(head);
+    if (head) useHead(head); // needs to be in root level with script setup
   }
 } catch (e) {
   console.log(e);
--- composables/useSeoMeta.ts
+++ composables/useSeoMeta.ts
@@ -1,6 +1,25 @@
+import { useCounterStore } from '@/stores/counterStore';
 import { Event } from '@/types/LiveEvents';
+import { storeToRefs } from 'pinia';
 
 export function useSeoMeta() {
-  const setHead = async () => {};
+  const setHead = async () => {
+    const route = useRoute();
+
+    const { events } = storeToRefs(useCounterStore());
+
+    if (route.name === 'Test') {
+      const data = await $fetch(`${import.meta.env.VITE_API_URL}/search/liveEvents`, {
+        body: { eventIds: 'ugn29046xzrbvyv0fbif9' },
+        method: 'POST',
+      });
+
+      events.value = (data as { events: { rows: [Event] } })?.events?.rows;
+
+      return {
+        title: events.value?.[0]?.event?.eventName ?? 'Head',
+      };
+    }
+  };
   return { setHead };
 }
